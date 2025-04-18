ff7bcc7db96e0aec9e2da3967a4ad53eebab6274
tsilavina007 <andriamiart114@gmail.com>
2025-04-18 13:01:19 +0300

    feat: Authorization ApiKey

---
+++ src/main/java/hei/spring/todo/security/ApiKeyFilter.java
@@ -0,0 +1,34 @@
+package hei.spring.todo.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${apikey}")
+    private String apiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+	System.out.println(header);
+        if (header == null || !header.equals("ApiKey " + apiKey)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
